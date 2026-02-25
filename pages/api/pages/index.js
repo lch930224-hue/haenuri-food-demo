f602daa773fa0ba45f2765c7d54552478f5719da
lch930224-hue <lch930224@gmail.com>
2026-02-25 22:28:56 +0900

    Create index.js

---
+++ pages/api/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:"sans-serif", padding:"20px", maxWidth:"600px", margin:"auto"}}>
+      <h1>🍽 해누리 식단 데모</h1>
+      <p>배포 성공! 다음 단계로 UI 연결 진행합니다.</p>
+    </div>
+  );
+}
